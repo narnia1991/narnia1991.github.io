10fbddb8207d2cb4de1f188cb14214d99ef668a2
Junar Alinsub <junaralinsub2@protonmail.com>
2023-06-04 10:40:30 +0800

    :sparkles: Add weaving webs and wife's website

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,20 @@ function App() {
   return (
     <div className="App">
       Been tinkering around the web since 2015. It's just now that I had
-      interest in creating my own.
+      interest in creating my own. <br />
+      <br />
+      Me and{" "}
+      <a href="https://jeanaica.com" target="_blank" rel="noreferrer noopener">
+        my wife
+      </a>{" "}
+      also held a talk last Aug 2022 with the PLM students. Presentation{" "}
+      <a
+        href="https://narnia1991.github.io/weaving-webs"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        here
+      </a>
     </div>
   );
 }
